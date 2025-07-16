e498aeb0419e855fedcc5bed8993af6e4aad4e75
Bao1404 <lethebao1404@gmail.com>
2025-07-17 00:36:43 +0700

    commit all

---
+++ Repositories/Repositories/AiConversationRepository.cs
@@ -0,0 +1,43 @@
+﻿using BusinessObjects;
+using DataAccessObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class AiConversationRepository : IAiConversationRepository
+    {
+        private readonly HealthCareSystemContext _context;
+        public AiConversationRepository(HealthCareSystemContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateConversation(Aiconversation conversation)
+        {
+            try
+            {
+                await _context.Aiconversations.AddAsync(conversation);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public async Task<Aiconversation> GetConversationByUserId(int userId)
+        {
+            try
+            {
+                return await _context.Aiconversations.FirstOrDefaultAsync(c => c.UserId == userId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
